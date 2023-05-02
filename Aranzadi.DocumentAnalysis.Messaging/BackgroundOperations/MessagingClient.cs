5c8943eefb22f461e2366ce76704fbc08c228859
DavidSanchezTR <david.sanchezfuentes@thomsonreuters.com>
2023-05-02 11:54:37 +0200

    Test

--- Aranzadi.DocumentAnalysis.Messaging/BackgroundOperations/MessagingClient.cs
+++ Aranzadi.DocumentAnalysis.Messaging/BackgroundOperations/MessagingClient.cs
@@ -1,265 +1,267 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Net.Http;
-//using System.Runtime.CompilerServices;
-//using System.Text;
-//using System.Threading.Tasks;
-//using ThomsonReuters.BackgroundOperations.Messaging;
-//using ThomsonReuters.BackgroundOperations.Messaging.Models;
-//using System.Linq;
-//using System.Transactions;
-//using Aranzadi.DocumentAnalysis.DTO.Request;
-//using Aranzadi.DocumentAnalysis.DTO.Response;
-//using Aranzadi.DocumentAnalysis.DTO;
-//using Microsoft.Extensions.Azure;
-//using System.Collections.Specialized;
-//using Newtonsoft.Json;
-//using Polly;
-//using System.Diagnostics;
-//using Polly.Retry;
-//using Microsoft.Extensions.Logging;
-//using log4net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ThomsonReuters.BackgroundOperations.Messaging;
+using ThomsonReuters.BackgroundOperations.Messaging.Models;
+using System.Linq;
+using System.Transactions;
+using Aranzadi.DocumentAnalysis.DTO.Request;
+using Aranzadi.DocumentAnalysis.DTO.Response;
+using Aranzadi.DocumentAnalysis.DTO;
+using Microsoft.Extensions.Azure;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+using Polly;
+using System.Diagnostics;
+using Polly.Retry;
+using Microsoft.Extensions.Logging;
+using log4net;
 
-//namespace Aranzadi.DocumentAnalysis.Messaging.BackgroundOperations
-//{
-//    /// <summary>
-//    /// 
-//    /// </summary>
-//    internal class MessagingClient : IClient
-//    {
+namespace Aranzadi.DocumentAnalysis.Messaging.BackgroundOperations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class MessagingClient : IClient
+    {
 
-//        private readonly IMessageSender messageSender;
-//        private readonly MessagingConfiguration confi;
+        private readonly IMessageSender messageSender;
+        private readonly MessagingConfiguration confi;
 
-//        private readonly IHttpClientFactory httpCliFact;
-//		private readonly ILog logger;
-//		internal static readonly string CLIENT_ID = "SAD";
+        private readonly IHttpClientFactory httpCliFact;
+        private readonly ILog logger;
+        internal static readonly string CLIENT_ID = "SAD";
 
-//        /// <summary>
-//        /// In the unit test, we don't want to wait some minutes, so we decrease the Max_time_polly_Retry to 1 second,       
-//        ///  
-//        /// </summary>
-//        internal static int MAX_TIME_POLLY_RETRY = 60;
+        /// <summary>
+        /// In the unit test, we don't want to wait some minutes, so we decrease the Max_time_polly_Retry to 1 second,       
+        ///  
+        /// </summary>
+        internal static int MAX_TIME_POLLY_RETRY = 60;
 
-//        internal static int N_TIMES_POLLY_RETRY = 5;
+        internal static int N_TIMES_POLLY_RETRY = 5;
 
-//        /// <summary>
-//        /// 
-//        /// </summary>
-//        /// <param name="messageSender"></param>
-//        /// <param name="confi"></param>
-//        /// <param name="cli"></param>
-//        /// <exception cref="ArgumentNullException">If some of the parameters is null</exception>
-//        internal MessagingClient(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli)
-//        {
-//            ValidateConstructorParameters(messageSender, confi, cli);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="messageSender"></param>
+        /// <param name="confi"></param>
+        /// <param name="cli"></param>
+        /// <exception cref="ArgumentNullException">If some of the parameters is null</exception>
+        internal MessagingClient(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli)
+        {
+            ValidateConstructorParameters(messageSender, confi, cli);
 
-//            this.messageSender = messageSender;
-//            this.confi = confi;
-//            this.httpCliFact = cli;
-//        }
+            this.messageSender = messageSender;
+            this.confi = confi;
+            this.httpCliFact = cli;
+        }
 
-//		internal MessagingClient(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli, ILog logger) : this(messageSender, confi, cli)
-//		{
-//			this.logger = logger;
-//		}
+        internal MessagingClient(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli, ILog logger) : this(messageSender, confi, cli)
+        {
+            this.logger = logger;
+        }
 
-//		private static void ValidateConstructorParameters(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli)
-//        {
-//            if (messageSender == null)
-//            {
-//                throw new ArgumentNullException(nameof(messageSender));
-//            }
-//            if (confi == null)
-//            {
-//                throw new ArgumentNullException(nameof(confi));
-//            }
-//            if (!confi.Validate())
-//            {
-//                throw new ArgumentException(nameof(confi));
-//            }
-//            if (cli == null)
-//            {
-//                throw new ArgumentNullException(nameof(cli));
-//            }
-//        }
+        private static void ValidateConstructorParameters(IMessageSender messageSender, MessagingConfiguration confi, IHttpClientFactory cli)
+        {
+            if (messageSender == null)
+            {
+                throw new ArgumentNullException(nameof(messageSender));
+            }
+            if (confi == null)
+            {
+                throw new ArgumentNullException(nameof(confi));
+            }
+            if (!confi.Validate())
+            {
+                throw new ArgumentException(nameof(confi));
+            }
+            if (cli == null)
+            {
+                throw new ArgumentNullException(nameof(cli));
+            }
+        }
 
-//        public async Task<PackageRequestTrack> SendRequestAsync(PackageRequest theRequest)
-//        {
-//            ValidateRequest(theRequest);
+        public async Task<PackageRequestTrack> SendRequestAsync(PackageRequest theRequest)
+        {
+            ValidateRequest(theRequest);
 
-//			logger?.Info($"Sending message to {confi.ServicesBusCola} with uid {theRequest.PackageUniqueRefences}");
+            logger?.Info($"Sending message to {confi.ServicesBusCola} with uid {theRequest.PackageUniqueRefences}");
 
-//			try
-//            {
-//                Message<DocumentAnalysisRequest> message = PrepareMessage(theRequest);
+            try
+            {
+                Message<DocumentAnalysisRequest> message = PrepareMessage(theRequest);
 
-//                await messageSender.Send(this.confi.ServicesBusCola, message);
+                await messageSender.Send(this.confi.ServicesBusCola, message);
 
-//                return CalculateTrack(message); ;
-//            }
-//            catch (Exception ex)
-//            {
-//				logger?.Error("Error while sending", ex);
-//				throw new DocumentAnalysisException("Error Enviando mensaje", ex);
-//            }
+                return CalculateTrack(message); ;
+            }
+            catch (Exception ex)
+            {
+                logger?.Error("Error while sending", ex);
+                throw new DocumentAnalysisException("Error Enviando mensaje", ex);
+            }
 
-//        }
-
-
-//        private static void ValidateRequest(PackageRequest theRequest)
-//        {
-//            if (theRequest == null)
-//            {
-//                throw new ArgumentNullException(nameof(theRequest));
-//            }
-//            if (!theRequest.Validate())
-//            {
-//                throw new ArgumentException(nameof(theRequest));
-//            }
-//        }
-
-//        private Message<DocumentAnalysisRequest> PrepareMessage(PackageRequest theRequest)
-//        {
-//            Message<DocumentAnalysisRequest> message;
-//            var dataChunks = theRequest.Documents.Select(
-//                            doc => new MessageDataChunk<DocumentAnalysisRequest>(doc));
-
-//            message = new Message<DocumentAnalysisRequest>(
-//               confi.Source, confi.Type, theRequest.Context.Tenant, dataChunks)
-//            {
-//                AdditionalData = theRequest.Context
-//            };
-//            return message;
-//        }
-
-//        private static PackageRequestTrack CalculateTrack(Message<DocumentAnalysisRequest> message)
-//        {
-//            PackageRequestTrack track = new PackageRequestTrack(){
-//                TrackingNumber = message.ID
-//            };
-//            var tracDetail = message.DataChunks.Select(chu => new DocumentAnalysisRequestTrack()
-//            {
-//                TrackingNumber = chu.ID,
-//                DocumentUniqueRefences = chu.Data.Guid
-//            });
-//            track.DocumentAnalysysRequestTracks = tracDetail;
-//            return track;
-//        }
-
-//        public async Task<DocumentAnalysisResponse> GetAnalysisAsync(AnalysisContext context, string documentId)
-//        {
-//            if (string.IsNullOrWhiteSpace(documentId))
-//            {
-//                throw new ArgumentNullException(nameof(documentId));
-//            }            
-//            var list = await RequestStatusList(context, documentId);
-//            if (list.Count>1)
-//            {
-//                throw new DocumentAnalysisException("There are more than one documento with id: " + documentId);
-//            }
-//            return list.FirstOrDefault();
-
-//        }       
-              
-
-//        public async Task<IEnumerable<DocumentAnalysisResponse>> GetAnalysisAsync(AnalysisContext context)
-//        {            
-
-//            return await RequestStatusList(context,string.Empty);
-//        }
-
-//        private async Task<List<DocumentAnalysisResponse>> RequestStatusList(AnalysisContext context, string docREf)
-//        {
-//            ValidateGetAnalysisContext(context);            
-           
-//            using (var httpCli = this.httpCliFact.CreateClient(CLIENT_ID))
-//            {
-//                try
-//                {
-//                    StatusRequest theStatusRequest = new StatusRequest()
-//                    {
-//                        App = context.App,
-//                        Tenant = context.Tenant,
-//                        Owner = context.Owner,
-//                        Hash = docREf,
-//                    };
-//                    List<DocumentAnalysisResponse> doc = new List<DocumentAnalysisResponse>();
-//                    Uri theUri = GetUri(theStatusRequest);
-//                    AsyncRetryPolicy policy = GetRetryPolicy(theStatusRequest);
-//                    string jsonDocAnalisis = await policy.ExecuteAsync(async () =>
-//                    {
-//                        return await httpCli.GetStringAsync(theUri);
-//                    });
-
-//                    if (!string.IsNullOrEmpty(jsonDocAnalisis))
-//                    {
-//                        doc = JsonConvert.DeserializeObject<List<DocumentAnalysisResponse>>(
-//                                       jsonDocAnalisis ?? String.Empty);
-//                    }
-//                    return doc;
-//                }
-//                catch (Exception ex)
-//                {
-//                    throw new DocumentAnalysisException("Error recuperando Status", ex);
-//                }                
-//            }
-//        }
-
-//        private static void ValidateGetAnalysisContext(AnalysisContext context)
-//        {
-//            if (context == null)
-//            {
-//                throw new ArgumentNullException(nameof(context));
-//            }
-//            if (!context.Validate())
-//            {
-//                throw new ArgumentException(nameof(context));
-//            }
-//        }
-
-//        private Uri GetUri(StatusRequest re)
-//        {
-//            NameValueCollection queryString;
-           
-//            queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
-//            queryString.Add(nameof(re.App), re.App);
-//            queryString.Add(nameof(re.Owner), re.Owner);
-//            queryString.Add(nameof(re.Tenant), re.Tenant);
-//            queryString.Add(nameof(re.Hash), re.Hash);
-
-//            UriBuilder ur = new UriBuilder(confi.URLServicioAnalisisDoc)
-//            {
-//                Query = queryString.ToString()
-//            };
-//            return ur.Uri;
-//        }
-
-//        private Polly.Retry.AsyncRetryPolicy GetRetryPolicy(StatusRequest theStatusRequest)
-//        {
-//            if (N_TIMES_POLLY_RETRY < 0)
-//                throw new DocumentAnalysisException("BAD POLLY Configuration, Nº Time Polly Retry: " + N_TIMES_POLLY_RETRY);
-
-//            var policy = Policy.Handle<HttpRequestException>().WaitAndRetryAsync(
-//                retryCount: N_TIMES_POLLY_RETRY,
-//                sleepDurationProvider: (attemptNum) => {                
-//                    return TimeSpan.FromSeconds(Math.Min(Math.Pow(2, attemptNum), MAX_TIME_POLLY_RETRY));
-//                },
-//                onRetry: (ex, ts) =>
-//                {
-//                    if (!String.IsNullOrEmpty(theStatusRequest.Hash))
-//                    {
-//                        Debug.WriteLine(ex, "Repetimos el documento: " + theStatusRequest.Hash);
-//                    }
-//                    else
-//                    {
-//                        Debug.WriteLine(ex, "Repetimos sin documento: ");
-//                    }
-//                });
-//            return policy;
-//        }
+        }
 
 
-//    }
+        private static void ValidateRequest(PackageRequest theRequest)
+        {
+            if (theRequest == null)
+            {
+                throw new ArgumentNullException(nameof(theRequest));
+            }
+            if (!theRequest.Validate())
+            {
+                throw new ArgumentException(nameof(theRequest));
+            }
+        }
 
-//}
+        private Message<DocumentAnalysisRequest> PrepareMessage(PackageRequest theRequest)
+        {
+            Message<DocumentAnalysisRequest> message;
+            var dataChunks = theRequest.Documents.Select(
+                            doc => new MessageDataChunk<DocumentAnalysisRequest>(doc));
+
+            message = new Message<DocumentAnalysisRequest>(
+               confi.Source, confi.Type, theRequest.Context.Tenant, dataChunks)
+            {
+                AdditionalData = theRequest.Context
+            };
+            return message;
+        }
+
+        private static PackageRequestTrack CalculateTrack(Message<DocumentAnalysisRequest> message)
+        {
+            PackageRequestTrack track = new PackageRequestTrack()
+            {
+                TrackingNumber = message.ID
+            };
+            var tracDetail = message.DataChunks.Select(chu => new DocumentAnalysisRequestTrack()
+            {
+                TrackingNumber = chu.ID,
+                DocumentUniqueRefences = chu.Data.Guid
+            });
+            track.DocumentAnalysysRequestTracks = tracDetail;
+            return track;
+        }
+
+        public async Task<DocumentAnalysisResponse> GetAnalysisAsync(AnalysisContext context, string documentId)
+        {
+            if (string.IsNullOrWhiteSpace(documentId))
+            {
+                throw new ArgumentNullException(nameof(documentId));
+            }
+            var list = await RequestStatusList(context, documentId);
+            if (list.Count > 1)
+            {
+                throw new DocumentAnalysisException("There are more than one documento with id: " + documentId);
+            }
+            return list.FirstOrDefault();
+
+        }
+
+
+        public async Task<IEnumerable<DocumentAnalysisResponse>> GetAnalysisAsync(AnalysisContext context)
+        {
+
+            return await RequestStatusList(context, string.Empty);
+        }
+
+        private async Task<List<DocumentAnalysisResponse>> RequestStatusList(AnalysisContext context, string docREf)
+        {
+            ValidateGetAnalysisContext(context);
+
+            using (var httpCli = this.httpCliFact.CreateClient(CLIENT_ID))
+            {
+                try
+                {
+                    StatusRequest theStatusRequest = new StatusRequest()
+                    {
+                        App = context.App,
+                        Tenant = context.Tenant,
+                        Owner = context.Owner,
+                        Hash = docREf,
+                    };
+                    List<DocumentAnalysisResponse> doc = new List<DocumentAnalysisResponse>();
+                    Uri theUri = GetUri(theStatusRequest);
+                    AsyncRetryPolicy policy = GetRetryPolicy(theStatusRequest);
+                    string jsonDocAnalisis = await policy.ExecuteAsync(async () =>
+                    {
+                        return await httpCli.GetStringAsync(theUri);
+                    });
+
+                    if (!string.IsNullOrEmpty(jsonDocAnalisis))
+                    {
+                        doc = JsonConvert.DeserializeObject<List<DocumentAnalysisResponse>>(
+                                       jsonDocAnalisis ?? String.Empty);
+                    }
+                    return doc;
+                }
+                catch (Exception ex)
+                {
+                    throw new DocumentAnalysisException("Error recuperando Status", ex);
+                }
+            }
+        }
+
+        private static void ValidateGetAnalysisContext(AnalysisContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (!context.Validate())
+            {
+                throw new ArgumentException(nameof(context));
+            }
+        }
+
+        private Uri GetUri(StatusRequest re)
+        {
+            NameValueCollection queryString;
+
+            queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
+            queryString.Add(nameof(re.App), re.App);
+            queryString.Add(nameof(re.Owner), re.Owner);
+            queryString.Add(nameof(re.Tenant), re.Tenant);
+            queryString.Add(nameof(re.Hash), re.Hash);
+
+            UriBuilder ur = new UriBuilder(confi.URLServicioAnalisisDoc)
+            {
+                Query = queryString.ToString()
+            };
+            return ur.Uri;
+        }
+
+        private Polly.Retry.AsyncRetryPolicy GetRetryPolicy(StatusRequest theStatusRequest)
+        {
+            if (N_TIMES_POLLY_RETRY < 0)
+                throw new DocumentAnalysisException("BAD POLLY Configuration, Nº Time Polly Retry: " + N_TIMES_POLLY_RETRY);
+
+            var policy = Policy.Handle<HttpRequestException>().WaitAndRetryAsync(
+                retryCount: N_TIMES_POLLY_RETRY,
+                sleepDurationProvider: (attemptNum) =>
+                {
+                    return TimeSpan.FromSeconds(Math.Min(Math.Pow(2, attemptNum), MAX_TIME_POLLY_RETRY));
+                },
+                onRetry: (ex, ts) =>
+                {
+                    if (!String.IsNullOrEmpty(theStatusRequest.Hash))
+                    {
+                        Debug.WriteLine(ex, "Repetimos el documento: " + theStatusRequest.Hash);
+                    }
+                    else
+                    {
+                        Debug.WriteLine(ex, "Repetimos sin documento: ");
+                    }
+                });
+            return policy;
+        }
+
+
+    }
+
+}
